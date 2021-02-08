8ac4ba66787c6b089f3fd17dbbd43ca94e9f08ab
jaime.zapata <jaime.zapata@globant.com>
2021-02-08 17:10:49 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aa513be941a035335b6356aa66929d51'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd3b2c222-1256-49d4-904f-156edbc05948'
+};
